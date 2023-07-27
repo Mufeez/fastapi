945ef03f3f7475e8b09d9931e84d035cd9bd5723
Mohammad Mufeez <mufeez.ahmed22@gmail.com>
2023-07-27 16:34:56 +0530

    adding delete and update endpoint

--- sensor/main.py
+++ sensor/main.py
@@ -1,26 +1,19 @@
 from typing import Optional, Union
 
-from fastapi import FastAPI ,Depends
-
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-
-from . import models,schemas
-
-from .database import SessionLocal,engine
-
+from . import models, schemas
+from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-   
-
 
 app = FastAPI()
 
@@ -28,24 +21,42 @@ app = FastAPI()
 def read_root():
     return "Server Is Running"
 
-
-
-
-@app.post("/sensor")
-def create_sensor(request:schemas.Sensor,db:Session=Depends(get_db)):
-    new_sensor=models.Sensor(id=request.id,temprature=request.temprature,location=request.location,status=request.status)
+@app.post("/sensor", response_model=schemas.Sensor)
+def create_sensor(request: schemas.Sensor, db: Session = Depends(get_db)):
+    new_sensor = models.Sensor(id=request.id, temprature=request.temprature, location=request.location, status=request.status)
     db.add(new_sensor)
     db.commit()
     db.refresh(new_sensor)
     return new_sensor
 
-@app.get('/sensors')
-def all(db:Session=Depends(get_db)):
-    sensors= db.query(models.Sensor).all()
+@app.get('/sensors', response_model=list[schemas.Sensor])
+def all(db: Session = Depends(get_db)):
+    sensors = db.query(models.Sensor).all()
     return sensors
 
+@app.get('/sensors/{id}', response_model=schemas.Sensor)
+def show(id: int, db: Session = Depends(get_db)):
+    sensor = db.query(models.Sensor).filter(models.Sensor.id == id).first()
+    if not sensor:
+        raise HTTPException(status_code=404, detail="Sensor not found")
+    return sensor
 
-@app.get('/sensors/{id}')
-def show(id:int,db:Session=Depends(get_db)):
-    sensor= db.query(models.Sensor).filter(models.Sensor.id == id).first()
+@app.put('/sensors/{id}', response_model=schemas.Sensor)
+def update_sensor(id: int, request: schemas.Sensor, db: Session = Depends(get_db)):
+    sensor = db.query(models.Sensor).filter(models.Sensor.id == id).first()
+    if not sensor:
+        raise HTTPException(status_code=404, detail="Sensor not found")
+    for key, value in request.dict(exclude_unset=True).items():
+        setattr(sensor, key, value)
+    db.commit()
+    db.refresh(sensor)
+    return sensor
+
+@app.delete('/sensors/{id}', response_model=schemas.Sensor)
+def delete_sensor(id: int, db: Session = Depends(get_db)):
+    sensor = db.query(models.Sensor).filter(models.Sensor.id == id).first()
+    if not sensor:
+        raise HTTPException(status_code=404, detail="Sensor not found")
+    db.delete(sensor)
+    db.commit()
     return sensor
